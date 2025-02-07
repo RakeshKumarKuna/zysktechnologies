bbfa786165a769db3d48e4720e30810b2da4574e
Rakesh Kumar Kuna <kunarakeshkumar@gmail.com>
2025-02-07 17:29:18 +0530

    Testimonal component completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Features } from './Components/Features.jsx';
 import './Components/HeaderSection.jsx'
 import { HeaderSection } from './Components/HeaderSection.jsx';
 import { Section } from './Components/Section.jsx';
+import { Testimonal } from './Components/Testimonal.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <HeaderSection></HeaderSection>
      <Section></Section>
      <Features></Features>
+     <Testimonal></Testimonal>
     </div>
   );
 }
---
+++ src/Components/Testimonal.jsx
@@ -0,0 +1,13 @@
+import './Testimonal.css'
+export function Testimonal(){
+    return (<div className='text-center mt-5'>
+        <div><img src='Assets/logo6.png'></img></div>
+        <div className='mt-2'><h3>We’ve been using Untitled to kick start every new project and can’t imagine working without it.</h3></div>
+        <div className='text-center'>
+            <div><img src='Assets/Avatar2.png'></img></div>
+            <div><span className='fw-bold'>Candice Wu</span></div>
+            <div><span style={{color:"#475467"}}>Product Manager, Sisyphus</span></div>
+
+        </div>
+    </div>)
+}
