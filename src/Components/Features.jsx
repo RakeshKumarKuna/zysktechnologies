f13f9f900618b039cbfba7cae963317ba6933d2f
Rakesh Kumar Kuna <kunarakeshkumar@gmail.com>
2025-02-07 16:59:30 +0530

    fetures component alignment fixed

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -10,17 +10,17 @@ export function Features() {
             <div >
                 <div>
                     <div className='first-row'>
-                        <div>
+                        <div className='text-center'>
                             <div className='text-center'><i class="bi bi-chat-dots"></i></div>
                             <div><span className='head-text'>Share team inboxes</span></div>
                             <div><span className='content-text'>Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop.</span></div>
                         </div>
-                        <div>
+                        <div className='text-center'>
                             <div className='text-center'><i class="bi bi-lightning-charge"></i></div>
                             <div><span className='head-text'>Deliver instant answers</span></div>
                             <div><span className='content-text'>An all-in-one customer service platform that helps you balance everything your customers need to be happy.</span></div>
                         </div>
-                        <div>
+                        <div className='text-center'>
                             <div className='text-center'><i class="bi bi-box-arrow-up-right"></i></div>
                             <div><span className='head-text'>Manage your team with reports</span></div>
                             <div><span className='content-text'>Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks.</span></div>
@@ -29,18 +29,21 @@ export function Features() {
                 </div>
                 <div>
                     <div className='second-row'>
-                        <div>
-                            <div>logo</div>
-                            <div>heading</div>
-                            <div>content</div>
+                        <div className='text-center'>
+                            <div className='text-center'><i class="bi bi-emoji-smile"></i></div>
+                            <div><span span className='head-text'>Connect with customers</span></div>
+                            <div className='content-text'><span>Solve a problem or close a sale in real-time with chat. If no one is available, customers are seamlessly routed to email without confusion.</span></div>
                         </div>
-                        <div><div>logo</div>
-                            <div>heading</div>
-                            <div>content</div></div>
-                        <div><div>logo</div>
-                            <div>heading</div>
-                            <div>content</div></div>
-                    </div>
+                        <div className='text-center'>
+                            <div className='text-center'><i class="bi bi-command"></i></div>
+                            <div><span span className='head-text'>Connect the tools you already use</span></div>
+                            <div className='content-text'><span>Explore 100+ integrations that make your day-to-day workflow more efficient and familiar. Plus, our extensive developer tools.</span></div></div>
+                        <div className='text-center'>
+                            <div className='text-center'><i class="bi bi-chat-heart"></i></div>
+                            <div><span span className='head-text'>Our people make the difference</span></div>
+                            <div className='content-text'><span>We’re an extension of your customer service team, and all of our resources are free. Chat to our friendly team 24/7 when you need help.</span></div>
+                            </div>
+                         </div>
                 </div>
             </div>
         </div>
